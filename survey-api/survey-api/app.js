127615f49cb683863fc64406f7daa06b66d596ab
shey3132 <s527673132@gmail.com>
2026-07-21 02:05:26 +0300

    Add files via upload

---
+++ survey-api/survey-api/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const { router: adminRouter } = require('./routes/admin');
+const ivrRouter = require('./routes/ivr');
+const displayRouter = require('./routes/display');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Yemot can POST as form-encoded
+
+app.use('/admin', adminRouter);
+app.use('/ivr', ivrRouter);
+app.use('/display', displayRouter);
+
+app.get('/', (req, res) => res.send('Survey API is running'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Survey API listening on port ${PORT}`));
+
+module.exports = app;
